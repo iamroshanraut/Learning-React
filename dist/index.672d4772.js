16a5e461164a8a8ca43178f797c5f5bf45c74c70
iamroshanraut <86304763+iamroshanraut@users.noreply.github.com>
2023-11-14 21:10:07 +0530

    Delete dist directory

--- dist/index.672d4772.js
+++
@@ -1,5 +0,0 @@
-const parent = React.createElement("h1", {}, "Hello React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-
-//# sourceMappingURL=index.672d4772.js.map
